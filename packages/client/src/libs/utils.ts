8014a4bfe3f663f70a04d6183a03ca02c5e1fab3
pkmiya <xbzd.cnlv@icloud.com>
2024-03-12 01:59:39 +0900

    add format-money-utils

--- packages/client/src/libs/utils.ts
+++ packages/client/src/libs/utils.ts
@@ -1,3 +1,7 @@
 export const classNames = (...classes: (string | undefined)[]) => {
   return classes.filter(Boolean).join(' ');
 };
+
+export const formatMoney = (amount: number): string => {
+  return amount.toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' });
+};
