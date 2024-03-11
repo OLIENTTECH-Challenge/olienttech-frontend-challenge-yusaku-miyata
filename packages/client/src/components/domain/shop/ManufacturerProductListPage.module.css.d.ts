f8137b3152cfbcd972122dd8c95977335f893b45
pkmiya <xbzd.cnlv@icloud.com>
2024-03-11 13:41:28 +0900

    add style

---
+++ packages/client/src/components/domain/shop/ManufacturerProductListPage.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles:
+  & Readonly<{ "orderCell": string }>
+  & Readonly<{ "orderInput": string }>
+;
+export default styles;
+//# sourceMappingURL=./ManufacturerProductListPage.module.css.d.ts.map
