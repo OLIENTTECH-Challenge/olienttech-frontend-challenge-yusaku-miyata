38a6507cd3501a1c50b80b8711c9f5c0d22110b4
pkmiya <xbzd.cnlv@icloud.com>
2024-03-11 20:11:52 +0900

    refactor base modal components

--- packages/client/src/components/base/Modal.module.css.d.ts
+++ packages/client/src/components/base/Modal.module.css.d.ts
@@ -1,7 +1,9 @@
 declare const styles:
   & Readonly<{ "modal": string }>
   & Readonly<{ "overlay": string }>
-  & Readonly<{ "modalButton": string }>
+  & Readonly<{ "modalHeader": string }>
+  & Readonly<{ "modalBody": string }>
+  & Readonly<{ "modalFooter": string }>
 ;
 export default styles;
 //# sourceMappingURL=./Modal.module.css.d.ts.map
--- packages/client/src/components/base/Modal.tsx
+++ packages/client/src/components/base/Modal.tsx
@@ -1,6 +1,29 @@
 import styles from './Modal.module.css';
 import { ReactNode } from 'react';
-import { Button } from '@/components/base/Button';
+
+type ModalHeaderProps = {
+  children: ReactNode;
+};
+
+export const ModalHeader: React.FC<ModalHeaderProps> = ({ children }: ModalHeaderProps) => {
+  return <div className={styles.modalHeader}>{children}</div>;
+};
+
+type ModalBodyProps = {
+  children: ReactNode;
+};
+
+export const ModalBody: React.FC<ModalBodyProps> = ({ children }: ModalBodyProps) => {
+  return <div className={styles.modalBody}>{children}</div>;
+};
+
+type ModalFooterProps = {
+  children: ReactNode;
+};
+
+export const ModalFooter: React.FC<ModalFooterProps> = ({ children }: ModalFooterProps) => {
+  return <div className={styles.modalFooter}>{children}</div>;
+};
 
 type ModalProps = {
   children: ReactNode;
@@ -14,14 +37,7 @@ export const Modal: React.FC<ModalProps> = ({ children, isOpen, onClose }: Modal
   return (
     <>
       <div className={styles.overlay} onClick={onClose} />
-      <div className={styles.modal}>
-        {children}
-        <div className={styles.modalButton}>
-          <Button variant='outlined' onClick={onClose}>
-            閉じる
-          </Button>
-        </div>
-      </div>
+      <div className={styles.modal}>{children}</div>
     </>
   );
 };
