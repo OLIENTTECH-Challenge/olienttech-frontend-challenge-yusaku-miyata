0e2271e352b1f2a928b460fd9cc375b1a75da7a1
pkmiya <xbzd.cnlv@icloud.com>
2024-03-12 02:17:46 +0900

    add format-date-utils & update dependency

--- packages/client/src/libs/utils.ts
+++ packages/client/src/libs/utils.ts
@@ -1,7 +1,15 @@
+import { format } from 'date-fns';
+import { ja } from 'date-fns/locale';
+
 export const classNames = (...classes: (string | undefined)[]) => {
   return classes.filter(Boolean).join(' ');
 };
 
 export const formatMoney = (amount: number): string => {
   return amount.toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' });
+};
+
+export const formatDate = (isoString: string): string => {
+  const date = new Date(isoString);
+  return format(date, 'PPpp', { locale: ja });
 };
