ac296eb9a304dace1de4efdc7eb38708bea1e995
pkmiya <xbzd.cnlv@icloud.com>
2024-03-11 15:10:59 +0900

    add modal styling

--- packages/client/src/components/domain/shop/ManufacturerProductListPage.module.css.d.ts
+++ packages/client/src/components/domain/shop/ManufacturerProductListPage.module.css.d.ts
@@ -1,6 +1,7 @@
 declare const styles:
   & Readonly<{ "orderCell": string }>
   & Readonly<{ "orderInput": string }>
+  & Readonly<{ "modalButton": string }>
 ;
 export default styles;
 //# sourceMappingURL=./ManufacturerProductListPage.module.css.d.ts.map
